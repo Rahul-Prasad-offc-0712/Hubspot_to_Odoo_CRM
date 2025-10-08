138801fe63be9e8acdf0f4adf5b55f85dda611a5
Rahul Prasad <rahulprasad07122001@gmail.com>
2025-10-08 12:50:52 +0530

    Move secrets to .env and safe config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+HUBSPOT_API_KEY = os.getenv("HUBSPOT_API_KEY")
+FORM_IQL_GUID = os.getenv("FORM_IQL_GUID")
+FORM_MQL_GUID = os.getenv("FORM_MQL_GUID")
+ODOO_URL = os.getenv("ODOO_URL")
+ODOO_DB = os.getenv("ODOO_DB")
+ODOO_USERNAME = os.getenv("ODOO_USERNAME")
+ODOO_PASSWORD = os.getenv("ODOO_PASSWORD")
