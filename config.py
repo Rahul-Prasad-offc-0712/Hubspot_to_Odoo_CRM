f807dfb6e2df6e45b6dfb28b10a992179ed5ff90
Rahul-Prasad-offc-0712 <rahulprasad07122001@gmail.com>
2025-11-07 21:42:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-HUBSPOT_API_KEY = os.getenv("HUBSPOT_API_KEY")
+HUBSPOT_API_KEY = "83d2bf72-c22d-4285-bdc9-573dd825f16d"
 FORM_IQL_GUID = "c3bda1e4-7318-4b8d-be17-d0a1d61bb239"
 FORM_MQL_GUID = os.getenv("FORM_MQL_GUID")
 ODOO_URL = os.getenv("ODOO_URL")
